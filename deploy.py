1397b9e818a0a579dbe7b82d2dc289fba3ec51c6
keiksy <hsinyaotsai@foxmail.com>
2020-03-04 15:53:49 +0800

    new post

---
+++ deploy.py
@@ -0,0 +1,15 @@
+import os, sys
+
+if os.getcwd() != '/Users/cai/Blog/keiksyblog':
+    sys.exit("Place this script to Hugo root folder!")
+
+os.system('git add .')
+os.system('git commit -m \'new post\'')
+os.system('git push')
+os.system('hugo -d ~/Blog/public')
+
+os.chdir('../public')
+os.system('git add .')
+os.system('git commit -m \'new post\'')
+os.system('git push')
+
